8d78c49c6925141a5ed4c94cea0b9463354448e3
Abisin-Raj <abisinr201@gmail.com>
2026-03-14 12:44:00 +0530

    Fix: revert to Share.shareXFiles in settings_screen.dart

--- lib/features/settings/presentation/settings_screen.dart
+++ lib/features/settings/presentation/settings_screen.dart
@@ -563,7 +563,7 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
       final file = File('${tempDir.path}/momentum_backup_${DateFormat('yyyyMMdd').format(DateTime.now())}.json');
       await file.writeAsString(jsonString);
       
-      await SharePlus.shareXFiles([XFile(file.path)], text: 'Momentum Backup');
+      await Share.shareXFiles([XFile(file.path)], text: 'Momentum Backup');
       
     } catch (e) {
       if (mounted) {
