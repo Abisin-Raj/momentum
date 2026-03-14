3475d89968ff1eca5284df8443c6612dd24b065e
Abisin-Raj <abisinr201@gmail.com>
2026-03-14 12:08:00 +0530

    Update health_connect_provider.dart

--- lib/core/providers/health_connect_provider.dart
+++ lib/core/providers/health_connect_provider.dart
@@ -1,5 +1,4 @@
 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:health/health.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:drift/drift.dart' show Value;
