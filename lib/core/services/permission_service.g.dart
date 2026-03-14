f81e588bda67f877d1eff8bc3f5a552f8e6bd95a
Abisin-Raj <abisinr201@gmail.com>
2026-03-14 12:24:00 +0530

    Update permission_service.g.dart

--- lib/core/services/permission_service.g.dart
+++ lib/core/services/permission_service.g.dart
@@ -6,38 +6,110 @@ part of 'permission_service.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(PermissionsHandled)
+final permissionsHandledProvider = PermissionsHandledProvider._();
+
+final class PermissionsHandledProvider
+    extends $NotifierProvider<PermissionsHandled, bool> {
+  PermissionsHandledProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'permissionsHandledProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$permissionsHandledHash();
+
+  @$internal
+  @override
+  PermissionsHandled create() => PermissionsHandled();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(bool value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<bool>(value),
+    );
+  }
+}
+
 String _$permissionsHandledHash() =>
     r'c93a6b0d55745e5c259b1fe5b4c3a8bab8859ca2';
 
-/// See also [PermissionsHandled].
-@ProviderFor(PermissionsHandled)
-final permissionsHandledProvider =
-    AutoDisposeNotifierProvider<PermissionsHandled, bool>.internal(
-      PermissionsHandled.new,
-      name: r'permissionsHandledProvider',
-      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-          ? null
-          : _$permissionsHandledHash,
-      dependencies: null,
-      allTransitiveDependencies: null,
-    );
+abstract class _$PermissionsHandled extends $Notifier<bool> {
+  bool build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final ref = this.ref as $Ref<bool, bool>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<bool, bool>,
+              bool,
+              Object?,
+              Object?
+            >;
+    element.handleCreate(ref, build);
+  }
+}
 
-typedef _$PermissionsHandled = AutoDisposeNotifier<bool>;
+@ProviderFor(PermissionService)
+final permissionServiceProvider = PermissionServiceProvider._();
+
+final class PermissionServiceProvider
+    extends $NotifierProvider<PermissionService, void> {
+  PermissionServiceProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'permissionServiceProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$permissionServiceHash();
+
+  @$internal
+  @override
+  PermissionService create() => PermissionService();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(void value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<void>(value),
+    );
+  }
+}
+
 String _$permissionServiceHash() => r'f2a6d70917d49562f8ef5864b2d9c6e32549e3d1';
 
-/// See also [PermissionService].
-@ProviderFor(PermissionService)
-final permissionServiceProvider =
-    AutoDisposeNotifierProvider<PermissionService, void>.internal(
-      PermissionService.new,
-      name: r'permissionServiceProvider',
-      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-          ? null
-          : _$permissionServiceHash,
-      dependencies: null,
-      allTransitiveDependencies: null,
-    );
-
-typedef _$PermissionService = AutoDisposeNotifier<void>;
-// ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
+abstract class _$PermissionService extends $Notifier<void> {
+  void build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final ref = this.ref as $Ref<void, void>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<void, void>,
+              void,
+              Object?,
+              Object?
+            >;
+    element.handleCreate(ref, build);
+  }
+}
